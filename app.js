5117dfc719f4afe0a4c15997c161b4221560328f
Austin Lambert <atlambert11@gmail.com>
2020-10-20 21:35:27 -0500

    Got render function to work, and created html file

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+const employeeObj = [];
+
 const questions = [{
         type: "input",
         name: "name",
@@ -72,10 +74,12 @@ function person() {
         .prompt(questions)
         .then((data) => {
             if (data.new) {
+                employeeObj.push(data);
                 person();
             } else {
+                employeeObj.push(data);
                 console.log("Okay fine");
-                render(data);
+                fs.writeFileSync(outputPath, render(employeeObj), "utf8");
             }
         })
 }
